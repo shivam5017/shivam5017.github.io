548fd1c113871f490f5b3a2bbe2f5f1dc0d77f2f
shivam malik <shivammalik962@gmail.com>
2023-05-14 22:30:10 +0530

    added chat project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -14,7 +14,9 @@ import {
   SiReactrouter,
   SiJavascript,
   SiMongodb,
-  SiNodedotjs
+  SiNodedotjs,
+  SiTypescript,
+  SiNextdotjs
 } from "react-icons/si";
 import { DiCss3 } from "react-icons/di";
 import { VscJson } from "react-icons/vsc";
@@ -246,8 +248,58 @@ in Louisville, Kentucky, that specializes in fried chicken.
             </div>
           </div>
         </div>
+        {/* messagner apk */}
+        <div className="projects_container">
+          <div className="project">
+            <div className="project_videocontainer">
+              <div data-aos="fade-right">
+                <img
+                  src="https://user-images.githubusercontent.com/110033165/238199835-a5663b2b-7b54-4fc3-9785-dbdf313333ba.png"
+                  alt="messanger"
+                />
+              </div>
+            </div>
+           
+<div className="project_information" data-aos="fade-right">
+<h2>Messanger App</h2>
+<p>
+
+A Messenger Chat application to chat with your friends and family members or you can create groups with your loved ones
+</p>
+<div>
+  <FaReact />
+  <SiNextdotjs />
+  <SiHtml5 />
+  <DiCss3 />
+  <SiMongodb />
+  <SiTypescript />
+</div>
+<div>
+  <a
+    href="https://messenger-hazel-xi.vercel.app/"
+    target="_blank"
+    rel="noreferrer"
+  >
+    <span type="button" className="btn btn--outline onbt">
+      See this Live
+    </span>
+  </a>
+  <a
+    href="https://github.com/shivam5017/messenger"
+    target="_blank"
+    rel="noreferrer"
+  >
+    <span type="button" className="btn btn--outline">
+      View Code
+    </span>
+  </a>
+</div>
+</div>
+          </div>
+        </div>
         
       </div>
     </>
   );
 };
+
